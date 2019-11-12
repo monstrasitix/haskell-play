e65df027d204a4382e35a31197d0eb94a1d498ba
Dmitrijs Savostijanovs <57198147+monstrasitix@users.noreply.github.com>
2019-11-12 17:56:02 +0000

    Update points.lisp

--- points.lisp
+++ points.lisp
@@ -9,17 +9,17 @@
 
 (setq points_2
       (list
-       (make-point :x 1 :y 2)
-       (make-point :x 1 :y 8)))
+       (make-point :x 1.1 :y 2.2)
+       (make-point :x 1.1 :y 8.2)))
 
 
 (defun point-sum (point)
-    (+ (point-x point) (point-y point)))
+    (round (+ (point-x point) (point-y point))))
 
 
-(dolist (point points_1)
+(defun print-point (point)
     (format t "|~s|~%" (point-sum point)))
 
 
-(dolist (point points_2)
-    (format t "|~s|~%" (point-sum point)))
+(dolist (point points_1) (print-point point))
+(dolist (point points_2) (print-point point))
