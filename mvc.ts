3c5c9b05d474ee7286299dc14cafa474da715a76
Dmitrijs Savostijanovs <57198147+monstrasitix@users.noreply.github.com>
2023-04-20 23:50:28 +0100

    Update mvc.ts

--- mvc.ts
+++ mvc.ts
@@ -1,70 +1,66 @@
 console.clear();
 
-
 class CounterModel {
-	public count = 0;
+  public count = 0;
   public step = 1;
-  
+
   public increment() {
-  	this.count += this.step;
+    this.count += this.step;
   }
-  
+
   public decrement() {
-  	this.count -= this.step;
+    this.count -= this.step;
   }
 }
 
 class CounterView {
-	constructor(
-  	public model,
-    public element,
-  ) {}
- 
-	updateCount() {
-  	this.element.output.textContent = this.model.count;
+  constructor(public model, public element) {}
+
+  updateCount() {
+    this.element.output.textContent = this.model.count;
   }
 }
 
 class Controller {
-	on(eventName, elementName, callback) {
-  	this.view.element[elementName].addEventListener(eventName, callback);
+  on(eventName, elementName, callback) {
+    this.view.element[elementName].addEventListener(eventName, callback);
   }
-  
+
   off(eventName, elementName, callback) {
-  	this.view.element[elementName].removeEventListener(eventName, callback);
+    this.view.element[elementName].removeEventListener(eventName, callback);
   }
 }
 
 class CounterController extends Controller {
   constructor(public model, public view) {}
-  
+
   onIncrement = () => {
-	  this.model.increment();
+    this.model.increment();
     this.view.updateCount();
-  }
-  
+  };
+
   onDecrement = () => {
-	  this.model.decrement();
+    this.model.decrement();
     this.view.updateCount();
-  }
-  
+  };
+
   mount() {
-  	this.on('click', 'increment', this.onIncrement);
-    this.on('click', 'decrement', this.onDecrement);
+    this.on("click", "increment", this.onIncrement);
+    this.on("click", "decrement", this.onDecrement);
   }
-  
+
   unMount() {
-    this.off('click', 'increment', this.onIncrement);
-    this.off('click', 'decrement', this.onDecrement);
+    this.off("click", "increment", this.onIncrement);
+    this.off("click", "decrement", this.onDecrement);
   }
 }
 
 const model = new CounterModel();
 
 const view = new CounterView(model, {
-  output: document.getElementById('output'),
-  increment: document.getElementById('increment'),
-  decrement: document.getElementById('decrement'),
+  output: document.getElementById("output"),
+  increment: document.getElementById("increment"),
+  decrement: document.getElementById("decrement"),
 });
 
 const controller = new CounterController(model, view);
