600020c99c5dbb71666b76d2ec8a7f1163361016
Dmitrijs Savostijanovs <57198147+monstrasitix@users.noreply.github.com>
2023-04-20 21:31:28 +0100

    Update constructor-overload.hs

--- constructor-overload.hs
+++ constructor-overload.hs
@@ -28,20 +28,12 @@ instance Show Todo where
         (todoId todo)
         (todoDescription todo)
 
-todoNew :: Int -> String -> Todo
-todoNew x text
-    | x `mod` 2 == 0    = new (Complete x text)
-    | otherwise         = new (Incomplete x text)
-
-todoNewComplete :: Int -> String -> Todo
-todoNewComplete x text = Todo x text True
-
 todos :: [Todo]
-todos = [ todoNew 1 "Learn Haskell"
-        , todoNew 2 "Get a dog"
-        , todoNew 3 "Walk the dog"
-        , todoNew 4 "Prepare for brunch"
-        , todoNew 5 "Drink water"
+todos = [ new (Incomplete 1 "Learn Haskell")
+        , new (Complete 2 "Get a dog")
+        , new (Incomplete 3 "Walk the dog")
+        , new (Complete 4 "Prepare for brunch")
+        , new (Incomplete 5 "Drink water") 
         ]
 
 main :: IO ()
